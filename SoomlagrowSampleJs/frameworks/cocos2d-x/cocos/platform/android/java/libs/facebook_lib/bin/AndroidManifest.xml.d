16934baf7f9834fc4231496a9b6089e0eca1edc6
wanghuan <wanghuan1115@live.cn>
2015-11-11 15:54:56 +0800

    sdkbox update for js

--- SoomlagrowSampleJs/frameworks/cocos2d-x/cocos/platform/android/java/libs/facebook_lib/bin/AndroidManifest.xml.d
+++ SoomlagrowSampleJs/frameworks/cocos2d-x/cocos/platform/android/java/libs/facebook_lib/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/huangqiyu/work/cocos-service-center/plugins/facebook/proj.android/libs/facebook_lib/bin/AndroidManifest.xml : \
-/Users/huangqiyu/work/cocos-service-center/plugins/facebook/proj.android/libs/facebook_lib/AndroidManifest.xml \
+/private/var/jenkins/.jenkins/jobs/_release_1.4/workspace/plugins/facebook/proj.android/libs/facebook_lib/bin/AndroidManifest.xml : \
+/private/var/jenkins/.jenkins/jobs/_release_1.4/workspace/plugins/facebook/proj.android/libs/facebook_lib/AndroidManifest.xml \
