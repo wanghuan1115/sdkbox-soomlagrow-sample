4b12e46067e6722fbbf1dd5c2a792e8f45c96f14
wanghuan <wanghuan1115@live.cn>
2015-11-30 13:01:19 +0800

    sdkbox update for lua 1.5.0.2

--- SoomlagrowSampleLua/frameworks/cocos2d-x/cocos/platform/android/java/libs/facebook_lib/bin/AndroidManifest.xml.d
+++ SoomlagrowSampleLua/frameworks/cocos2d-x/cocos/platform/android/java/libs/facebook_lib/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/private/var/jenkins/.jenkins/jobs/_release_1.4.1/workspace/plugins/facebook/proj.android/libs/facebook_lib/bin/AndroidManifest.xml : \
-/private/var/jenkins/.jenkins/jobs/_release_1.4.1/workspace/plugins/facebook/proj.android/libs/facebook_lib/AndroidManifest.xml \
+/private/var/jenkins/.jenkins/jobs/csc_1.5_stage/workspace/plugins/facebook/proj.android/libs/facebook_lib/bin/AndroidManifest.xml : \
+/private/var/jenkins/.jenkins/jobs/csc_1.5_stage/workspace/plugins/facebook/proj.android/libs/facebook_lib/AndroidManifest.xml \
